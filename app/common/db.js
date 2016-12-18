37ffd4e15cbc8350a2bd7ba51d4a83161f655f74
Brandon Phillips <bericp1@gmail.com>
2016-12-18 02:43:38 -0700

    small bugfix

--- app/common/db.js
+++ app/common/db.js
@@ -3,10 +3,11 @@
 const Sequelize = require('sequelize');
 
 let connection_url;
+
 if(process.env.POKEDEX_EXPLORER_DATABASE_URL)
   connection_url = process.env.POKEDEX_EXPLORER_DATABASE_URL;
 else if(process.env.CLEARDB_DATABASE_URL)
-  connection_url = CLEARDB_DATABASE_URL;
+  connection_url = process.env.CLEARDB_DATABASE_URL;
 else
   connection_url = 'mysql://pokedexexplorer:pokedexexplorer@localhost/pokedex';
 
